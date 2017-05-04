0acd357bf54e7f58209ce2f05e8ec4720cabdfca
daunenok <daunenok@list.ru>
2017-05-04 22:01:31 +0600

    Add main content

---
+++ app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    getQuote();
+
+	$('#get-quote').on('click', function(event) {
+		event.preventDefault();	
+        getQuote();	
+	});
+});
+
+function getQuote() {
+$.ajax({url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+        success: function(data) {
+            var post = data.shift(); 
+            $('#quote-title').text(post.title);
+            $('#quote-content').html(post.content);
+            var twitText = "https://twitter.com/intent/tweet?text=" + 
+                           $('#quote-content').text() + " - " + 
+                           $('#quote-title').text();
+            twitText = twitText.replace(/ /g, "%20");
+            $(".twitter-share-button").attr("href", twitText);
+            console.log($(".twitter-share-button").attr("href"));
+        },
+        cache: false
+       });
+}
